1cb505bed77158b74892f8841eb3dcce396afbde
TZGaming <tomzuidervaart@gmail.com>
2026-09-03 14:57:32 +0200

    #3 No mugshot filter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,14 @@
     {#each data.squad as person}
         {@const firstName = person.name.split(' ')[0]}
         <li>
-            <img src="https://fdnd.directus.app/assets/{person.mugshot}?width=200" alt="{firstName}" width="65" height="65"/>
+            <img
+                src={person.mugshot
+                    ? `https://fdnd.directus.app/assets/${person.mugshot}?width=200`
+                    : '../src/lib/assets/unknown.webp'}
+                alt="{firstName}"
+                width="65"
+                height="65"
+            />
             <a href="/student/{person.id}">{firstName}</a>
         </li>
     {/each}
