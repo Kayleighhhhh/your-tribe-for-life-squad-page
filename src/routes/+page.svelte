1f53e08d1138246e3f59bf5849768a437f38e9a5
TZGaming <tomzuidervaart@gmail.com>
2026-09-03 13:55:01 +0200

    #3 Show student mugshots

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,11 @@
 
 <h1>Squad 2G</h1>
 
-{#each data.squad as person}
- <li><a href="/student/{person.id}">{person.name}</a></li>
-{/each}
+<ul>
+    {#each data.squad as person}
+    <li>
+        <img src="https://fdnd.directus.app/assets/{person.mugshot}?width=200" alt="{person.name}" width="65" height="65"/>
+        <a href="/student/{person.id}">{person.name}</a>
+    </li>
+    {/each}
+</ul>
