56b2f8f1dca1fca4f5982f47eb36faa01afa8cbd
TZGaming <tomzuidervaart@gmail.com>
2026-09-03 15:07:50 +0200

    #3 <a> wraps around as container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,17 @@
     {#each data.squad as person}
         {@const firstName = person.name.split(' ')[0]}
         <li>
-            <img
-                src={person.mugshot
-                    ? `https://fdnd.directus.app/assets/${person.mugshot}?width=200`
-                    : '../src/lib/assets/unknown.webp'}
-                alt="{firstName}"
-                width="65"
-                height="65"
-            />
-            <a href="/student/{person.id}">{firstName}</a>
+            <a href="/student/{person.id}">
+                <img
+                    src={person.mugshot
+                        ? `https://fdnd.directus.app/assets/${person.mugshot}?width=200`
+                        : '../src/lib/assets/unknown.webp'}
+                    alt="{firstName}"
+                    width="65"
+                    height="65"
+                />
+                <p>{firstName}</p>
+            </a>
         </li>
     {/each}
 </ul>
