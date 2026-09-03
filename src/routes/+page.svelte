e860179b60240a25eb418e7d072bb0855ac82c9f
TZGaming <tomzuidervaart@gmail.com>
2026-09-03 14:46:53 +0200

    #3 Desktop layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,16 @@
  let { data } = $props()
 </script>
 
-<h1>Squad 2G</h1>
-
 <ul>
     {#each data.squad as person}
-    <li>
-        <img src="https://fdnd.directus.app/assets/{person.mugshot}?width=200" alt="{person.name}" width="65" height="65"/>
-        <a href="/student/{person.id}">{person.name}</a>
-    </li>
+        {@const firstName = person.name.split(' ')[0]}
+        <li>
+            <img src="https://fdnd.directus.app/assets/{person.mugshot}?width=200" alt="{firstName}" width="65" height="65"/>
+            <a href="/student/{person.id}">{firstName}</a>
+        </li>
     {/each}
 </ul>
+
+<nav>
+    <!-- Hier komt de taakbalk -->
+</nav>
