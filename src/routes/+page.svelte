0a7d1c4ceea883d8dd06863aa10a905f008e4e91
SieuwkeSheta <sieuwke_sheta@hotmail.nl>
2026-09-08 18:50:24 +0200

    Changed routes from /student/[id] to /[personName] #20

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,5 @@
 <h1>Squad 2G</h1>
 
 {#each data.squad as person}
- <li><a href="/student/{person.id}">{person.name}</a></li>
+ <li><a href="/{person.name.split(' ')[0]}">{person.name}</a></li>
 {/each}
