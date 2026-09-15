fbfec9c9f6c6c6f0e42d087743fb785388393b03
SieuwkeSheta <sieuwke_sheta@hotmail.nl>
2026-09-15 10:47:28 +0200

    Fixed rerouting on detail page #20

--- src/routes/[personName]/+page.svelte
+++ src/routes/[personName]/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     // Receive the "data" prop from the load function ({ person })
 	let { data } = $props()
-    let { person } = $derived(data.person)
+    let { person } = $derived(data)
 </script>
 
 <article>
