4511272f195bbe099b4d28350836bc7694af1e4f
TZGaming <tomzuidervaart@gmail.com>
2026-09-17 11:45:29 +0200

    #36 added missing width & height attribute

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 		<div class="logo">
 			<picture>
 				<source srcset="../src/lib/assets/WindowsXP.avif" type="image/avif" />
-				<img src="../src/lib/assets/WindowsXP.png" alt="Windows XP" />
+				<img src="../src/lib/assets/WindowsXP.png" alt="Windows XP" width="50" height="50" />
 			</picture>
 			<h1>Squad 2G</h1>
 		</div>
