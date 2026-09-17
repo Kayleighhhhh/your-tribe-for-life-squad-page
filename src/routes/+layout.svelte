ef631986b798e9e789233154c69a34f02fa1f45f
SieuwkeSheta <sieuwke_sheta@hotmail.nl>
2026-09-17 13:10:18 +0200

    Changed `<h>` order on detail page with if-statement to check what page you're on #40

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
 	import favicon from '$lib/assets/WindowsXP.png';
 	import '$lib/style.css'
-	import { page } from '$app/stores';
+	import { page } from '$app/state'
+
+	// To check if you are on the homepage
+	const isHome = $derived(page.url.pathname === '/')
 
 	let { children } = $props();
 </script>
@@ -21,13 +24,17 @@
 				<source srcset="../src/lib/assets/WindowsXP.avif" type="image/avif" />
 				<img src="../src/lib/assets/WindowsXP.png" alt="Windows XP" width="50" height="50" />
 			</picture>
-			<h1>Squad 2G</h1>
+			{#if isHome}
+                <h1>Squad 2G</h1>
+            {:else}
+                <h2>Squad 2G</h2>
+            {/if}
 		</div>
-		{#if $page.data.person}
+		{#if page.data.person}
 		<section>
-			<h2>
-				{$page.data.person.name.split(' ')[0]}
-			</h2>
+			<p>
+				{page.data.person.name.split(' ')[0]}
+			</p>
 		</section>
 		{/if}
 	</nav>
--- src/routes/[personName]/+page.svelte
+++ src/routes/[personName]/+page.svelte
@@ -10,7 +10,7 @@
 
 <article>
     <header>
-        <h2>[{person.name}]</h2>
+        <h1>[{person.name}]</h1>
         <a href="/" class="nav-bar-icon">
             <img src="../src/lib/assets/minimize-xp-icon.png" alt="Go back to homepage" width="50" height="50">
         </a>
@@ -130,7 +130,7 @@
             }
         }
 
-        h2 {
+        h1 {
             color: var(--lightest-text-color);
             font-size: var(--font-size-m);
             font-weight: 500;
