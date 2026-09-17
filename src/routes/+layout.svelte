7cfb05786012d9fbe18d5f14f2be194ab532039d
TZGaming <tomzuidervaart@gmail.com>
2026-09-17 11:39:24 +0200

    #34 remove WebP conversions part 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,6 @@
 		<div class="logo">
 			<picture>
 				<source srcset="../src/lib/assets/WindowsXP.avif" type="image/avif" />
-				<source srcset="../src/lib/assets/WindowsXP.webp" type="image/webp" />
 				<img src="../src/lib/assets/WindowsXP.png" alt="WindowsXP logo" />
 			</picture>
 			<h1>Squad 2G</h1>
