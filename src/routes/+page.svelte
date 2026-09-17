4994ff2fc6c8d8c1e96366115b5558c56bdc7e95
TZGaming <tomzuidervaart@gmail.com>
2026-09-17 11:36:24 +0200

    #34 picture elements added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,14 +11,16 @@
         {@const firstName = person.name.split(' ')[0]}
         <li>
             <a href="/{firstName}">
-                <img
-                    src={person.mugshot
-                        ? `https://fdnd.directus.app/assets/${person.mugshot}?width=200`
-                        : '../src/lib/assets/unknown.webp'}
-                    alt="Mugshot of {firstName}"
-                    width="65"
-                    height="65"
-                />
+                <picture>
+                    {#if person.mugshot != null}
+                    <source srcset="https://fdnd.directus.app/assets/{person.mugshot}?width=200&format=webp" type="image/webp">
+                        <source srcset="https://fdnd.directus.app/assets/{person.mugshot}?width=200&format=avif" type="image/avif">
+                        <img loading="lazy" src="https://fdnd.directus.app/assets/{person.mugshot}?width=200" width="65" height="65" alt="Mugshot of {person.name}">
+
+                        {:else}
+                        <img src="../src/lib/assets/unknown.avif" width="65" height="65" alt="Placeholder of a questionmark">
+                    {/if}
+                </picture>
                 <p>{firstName}</p>
             </a>
         </li>
--- src/routes/[personName]/+page.svelte
+++ src/routes/[personName]/+page.svelte
@@ -18,14 +18,13 @@
 
     <figure>
         <picture>
-
             {#if person.mugshot != null}
                 <source srcset="https://fdnd.directus.app/assets/{person.mugshot}?width=800&format=avif" type="image/avif">
                 <source srcset="https://fdnd.directus.app/assets/{person.mugshot}?width=800&format=webp" type="image/webp">
                 <img loading="lazy" src="https://fdnd.directus.app/assets/{person.mugshot}?width=800" width="200" height="133" alt="Mugshot of {person.name}">
 
                 {:else}
-                <img src="../src/lib/assets/unknown.webp" width="200" height="200" alt="Placeholder of a questionmark">
+                <img src="../src/lib/assets/unknown.avif" width="200" height="200" alt="Placeholder of a questionmark">
             {/if}
         </picture>
         <figcaption>[{person.name}]</figcaption>
